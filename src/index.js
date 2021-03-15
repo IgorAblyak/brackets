d7cdc1b8d6cb8962391829f63928117e9b8297a6
IgorAblyak <iablyak@gmail.com>
2021-03-15 23:45:56 +0300

    feat: implement function for checking brackets sequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 != 0) return false;
+
+  let sumString = [];
+  for(let item of bracketsConfig) {
+    let subArray = item.join('');
+    sumString.push(subArray);
+  }
+
+  let refBrackets = sumString.join('');
+  let stack = []
+
+  for(let bracket of str) {
+    let bracketsIndex = refBrackets.indexOf(bracket)
+    if(bracketsIndex % 2 === 0) {
+      stack.push(bracketsIndex + 1)
+    } else {
+      if(stack.pop() !== bracketsIndex) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0
 }
